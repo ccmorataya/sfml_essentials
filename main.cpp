f93afd7743078e57bc043dd411e0a808c8ab8385
Cristian Morataya <cris.morataya@gmail.com>
2016-07-07 14:31:30 -0600

    Modularization of code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,59 @@
 #include <SFML/Graphics.hpp>
 
+class Game
+{
+	public:
+				Game();
+		void	run();
+
+	private:
+		void	processEvents();
+		void	update();
+		void	render();
+		sf::RenderWindow mWindow;
+		sf::CircleShape mPlayer;
+		sf::CircleShape mSqare;
+};
+
+Game::Game()
+	:mWindow(sf::VideoMode(640, 480), "SFML modularization"), mPlayer(), mSqare()
+{
+	mSqare.setRadius(40.f);
+	mSqare.setPosition(100.f, 100.f);
+	mSqare.setFillColor(sf::Color::Red);
+}
+
+void Game::run()
+{
+	while (mWindow.isOpen())
+	{
+		processEvents();
+		update();
+		render();
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent (event))
+	{
+		if (event.type == sf::Event::Closed) {
+			mWindow.close();
+		}
+	}
+}
+void Game::update(){}
+
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mSqare);
+	mWindow.display();
+}
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML application");
-	sf::CircleShape shape;
-	shape.setRadius(40.f);
-	shape.setPosition(100.f, 100.f);
-	shape.setFillColor(sf::Color::Cyan);
-
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+	Game game;
+	game.run();
 }
