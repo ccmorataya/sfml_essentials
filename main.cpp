6d7ecc92a838d56e12c8fd4e56b2ca0109fe8ee9
Cristian Morataya <cris.morataya@gmail.com>
2016-07-08 13:42:05 -0600

    Coded the movement with WASD, A isn't working

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ class Game
 	public:
 				Game();
 		void	run();
+		void	handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
 	private:
 		void	processEvents();
@@ -12,6 +13,10 @@ class Game
 		void	render();
 		sf::RenderWindow mWindow;
 		sf::CircleShape mPlayer;
+		bool	mIsMovingUp;
+		bool	mIsMovingDown;
+		bool	mIsMovingRight;
+		bool	mIsMovingLeft;
 };
 
 Game::Game()
@@ -37,12 +42,57 @@ void Game::processEvents()
 	sf::Event event;
 	while (mWindow.pollEvent (event))
 	{
+		switch (event.type) {
+			case sf::Event::KeyPressed:
+				handlePlayerInput(event.key.code, true);
+				break;
+			case sf::Event::KeyReleased:
+				handlePlayerInput(event.key.code, false);
+				break;
+			case sf::Event::Closed:
+				mWindow.close();
+				break;
+		}
 		if (event.type == sf::Event::Closed) {
 			mWindow.close();
 		}
 	}
 }
-void Game::update(){}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	if (key == sf::Keyboard::W) {
+		mIsMovingUp = isPressed;
+	}
+	else if (key == sf::Keyboard::S) {
+		mIsMovingDown = isPressed;
+	}
+	else if (key == sf::Keyboard::A) {
+		mIsMovingLeft = isPressed;
+	}
+	else if (key == sf::Keyboard::D) {
+		mIsMovingRight = isPressed;
+	}
+}
+
+void Game::update()
+{
+	sf::Vector2f movement(0.f, 0.f);
+	if (mIsMovingUp) {
+		movement.y -= 1.f;
+	}
+	if (mIsMovingDown) {
+		movement.y += 1.f;
+	}
+	if (mIsMovingRight)	{
+		movement.x += 1.f;
+	}
+	if (mIsMovingLeft){
+		movement.x -+ 1.f;
+	}
+
+	mPlayer.move(movement);
+}
 
 void Game::render()
 {
