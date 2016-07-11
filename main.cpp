dbc17b31ebd12181cb431ab2e390029897a3e2e4
Cristian Morataya <cris.morataya@gmail.com>
2016-07-11 16:38:33 -0600

    Use switch to handle the diferent keys of the keyboard

--- main.cpp
+++ main.cpp
@@ -9,9 +9,28 @@ int main()
 		sf::Event event;
 		while(window.pollEvent(event))
 		{
-			if(event.type == sf::Event::Closed)
-			{
-				window.close();
+			switch (event.type) {
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::KeyPressed:
+					if (event.key.code == sf::Keyboard::Space)
+					{
+						window.setTitle("Space");
+					}
+					break;
+				case sf::Event::KeyReleased:
+					if (event.key.code == sf::Keyboard::Space)
+					{
+						window.setTitle("Releases");
+					}
+					else if (event.key.code == sf::Keyboard::Escape)
+					{
+						window.close();
+					}
+					break;
+				default:
+					break;
 			}
 		}
 
