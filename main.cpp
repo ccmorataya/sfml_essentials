6c5b3fcc69277e9b8aed6a2c67cd33c0e7e22366
Cristian Morataya <cris.morataya@gmail.com>
2016-07-11 15:44:57 -0600

    Added the handling of the events with pollEvent

--- main.cpp
+++ main.cpp
@@ -3,6 +3,22 @@
 int main()
 {
 	sf::Window window (sf::VideoMode(300,200), "title");
-	sf::sleep(sf::seconds(3));
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		//update frame
+		
+		//render frame
+	}
+
 	return 0;
 }
